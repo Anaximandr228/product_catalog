cc4cd29f8a4a8bfaf44f1ad254b9c0b9b13d58b3
Anaximandr228 <alex09.g29@gmail.com>
2024-09-12 21:18:41 +0500

    add docker

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import os
 
-host = "localhost"  # Хост приложения
+host = os.environ.get('POSTGRES_HOST', 'localhost')  # Хост приложения
 user = "postgres"  # Имя пользователя PostgreSQL
 password = "rtf558Ur"  # Пароль пользователя PostgreSQL
 db_name = "zit_app"  # Название базы данных PostgeSQL
