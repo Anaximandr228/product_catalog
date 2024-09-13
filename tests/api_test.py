b599bff3c13158b099e9294a5af7dbe5bd358150
Anaximandr228 <alex09.g29@gmail.com>
2024-09-14 03:29:38 +0500

    add test

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,14 +1,92 @@
+import json
+from typing import Generator
+
+import psycopg2
+import pytest
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from starlette.testclient import TestClient
+
+import models
 from config import user, password, host
-from zit_app import read_products
+from zit_app import app, get_db
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{user}:{password}@{host}/test'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def test_get_products():
-    read_products()
+@pytest.fixture(scope="session")
+def connection():
+    con = psycopg2.connect(dbname='postgres',
+                           user=user, host=host,
+                           password=password)
+
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = con.cursor()
+    cur.execute('DROP DATABASE IF EXISTS test')
+    cur.execute("CREATE DATABASE test")
+
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+    )
+    models.Base.metadata.create_all(bind=engine)
+    return engine.connect()
+
+
+@pytest.fixture(scope="session")
+def db_session(connection):
+    transaction = connection.begin()
+    session_factory = sessionmaker(autocommit=False, autoflush=False, bind=connection)
+    session = session_factory()
+    yield session
+    transaction.rollback()
+
+
+@pytest.fixture(scope='module')
+def client(db_session) -> Generator:
+    def override_get_db():
+        yield db_session
+
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as c:
+        yield c
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture()
+def products_setup(db_session):
+    db_type = models.Product_type(name='test_type')
+    db_session.add(db_type)
+    db_session.commit()
+    db_product = models.Product(name='Test', product_type_id=1)
+    db_session.add(db_product)
+    db_session.commit()
+
+
+def test_get_products(client, products_setup):
+    print('test_get_products')
+    response = client.get("/products")
+    assert response.status_code == 200
+    assert response.json()[0]['name'] == 'Test'
+
+
+def test_get_product_id(client, products_setup):
+    print('test_get_products_id')
+    response = client.get("/products/1")
+    assert response.status_code == 200
+    assert response.json()[0]['name'] == 'Test'
+
+
+def test_post_products_type(client, products_setup):
+    print('test_get_products_type')
+    response = client.post("/products/type/1")
+    assert response.status_code == 200
+    assert response.json()[0]['name'] == 'Test'
+
+
+def test_post_product(client, products_setup):
+    print('test_post_products')
+    data = {"name": "Test"}
+    response = client.post("/products", data=json.dumps(data))
+    assert response.status_code == 200
+    # assert response.json()[0]['name'] == 'Test'
